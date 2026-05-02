4ad4975c5662137f9ddd3c394f61474453a1fb49
Swusan <bmoregarfield@gmail.com>
2026-05-02 13:33:29 -0400

    Added Slot Deletion

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,16 @@ type TimeSlotProps = {
     timeDifference: number
     sliderVal: number
     timeString: string
+    id: string
     handleChange: ChangeEventHandler<HTMLInputElement>
+    onRemove: (removeUUID: string) => void
 }
 
 type TimeSlotBoardProps = {
     slots: {name: string, offset: number, id: string}[]
     currentTime: number
     changeTime: (timeDiff:number) => ChangeEventHandler<HTMLInputElement>
+    onRemove: (removeUUID: string) => void
 }
 
 type InputFormProps = {
@@ -22,14 +25,22 @@ type InputFormProps = {
     onClearAll: () => void
 }
 
-function RemoveButton() {
+type RemoveButtonProps = {
+    onRemove: (removeUUID: string) => void
+    slotUUID: string
+}
+
+function RemoveButton({onRemove, slotUUID}: RemoveButtonProps) {
     const [isActive, setIsActive] = useState(false);
 
     return (
         <button
                 className={`absolute top-3 right-6 ${isActive ? "text-gray-600" : "text-[#949494]"}`}
                 type="button"
-                onClick={() => setIsActive(true)}
+                onClick={() => {
+                    setIsActive(true)
+                    onRemove(slotUUID)
+                }}
             >
                 <svg viewBox="0 0 456 511.82" width="21" fill="currentColor">
                     <path d="M48.42 140.13h361.99c17.36 0 29.82 9.78 28.08 28.17l-30.73 317.1c-1.23 13.36-8.99 26.42-25.3 26.42H76.34c-13.63-.73-23.74-9.75-25.09-24.14L20.79 168.99c-1.74-18.38 9.75-28.86 27.63-28.86zM24.49 38.15h136.47V28.1c0-15.94 10.2-28.1 27.02-28.1h81.28c17.3 0 27.65 11.77 27.65 28.01v10.14h138.66c.57 0 1.11.07 1.68.13 10.23.93 18.15 9.02 18.69 19.22.03.79.06 1.39.06 2.17v42.76c0 5.99-4.73 10.89-10.62 11.19-.54 0-1.09.03-1.63.03H11.22c-5.92 0-10.77-4.6-11.19-10.38 0-.72-.03-1.47-.03-2.23v-39.5c0-10.93 4.21-20.71 16.82-23.02 2.53-.45 5.09-.37 7.67-.37zm83.78 208.38c-.51-10.17 8.21-18.83 19.53-19.31 11.31-.49 20.94 7.4 21.45 17.57l8.7 160.62c.51 10.18-8.22 18.84-19.53 19.32-11.32.48-20.94-7.4-21.46-17.57l-8.69-160.63zm201.7-1.74c.51-10.17 10.14-18.06 21.45-17.57 11.32.48 20.04 9.14 19.53 19.31l-8.66 160.63c-.52 10.17-10.14 18.05-21.46 17.57-11.31-.48-20.04-9.14-19.53-19.32l8.67-160.62zm-102.94.87c0-10.23 9.23-18.53 20.58-18.53 11.34 0 20.58 8.3 20.58 18.53v160.63c0 10.23-9.24 18.53-20.58 18.53-11.35 0-20.58-8.3-20.58-18.53V245.66z"/>
@@ -38,10 +49,13 @@ function RemoveButton() {
     )
 }
 
-function TimeSlot({name, timeDifference, sliderVal, timeString, handleChange}: TimeSlotProps) {
+function TimeSlot({name, timeDifference, sliderVal, timeString, id, handleChange, onRemove}: TimeSlotProps) {
     return (
         <div className="px-12 py-4 bg-[rgb(255,83,83)] rounded-lg relative">
-            <RemoveButton />
+            <RemoveButton 
+                onRemove={onRemove}
+                slotUUID={id}
+            />
             <div className="p-3 text-center font-karla text-lg">{name} +{timeDifference} hr(s)</div>
             <div className="p-3 text-center">
                 <input
@@ -59,7 +73,7 @@ function TimeSlot({name, timeDifference, sliderVal, timeString, handleChange}: T
     )
 }
 
-function TimeSlotBoard({slots, currentTime, changeTime}: TimeSlotBoardProps) {
+function TimeSlotBoard({slots, currentTime, changeTime, onRemove}: TimeSlotBoardProps) {
     function toLocalTime(base: number, offset: number): number {
         return (base + offset * 60 + 1440) % 1440
     }
@@ -83,7 +97,9 @@ function TimeSlotBoard({slots, currentTime, changeTime}: TimeSlotBoardProps) {
                                     timeDifference={timeSlot.offset}
                                     sliderVal={toLocalTime(currentTime, timeSlot.offset)}
                                     timeString={getTime(toLocalTime(currentTime, timeSlot.offset))}
+                                    id={timeSlot.id}
                                     handleChange={changeTime(timeSlot.offset)}
+                                    onRemove={onRemove}
                                 />
                             </div>
                         )
@@ -212,6 +228,10 @@ function App() {
         setTimeSlots([]);
     }
 
+    const onRemove = (removeUUID: string) : void => {
+        setTimeSlots(timeSlots.filter(item => item.id != removeUUID))
+    }
+
     return (
     <>
         <div className={"w-screen text-center text-5xl font-bold tracking-widest font-zen-dots p-8 text-[rgb(255,83,83)]"}>- - - - PHONE CALL TIMING - - - -</div>
@@ -226,6 +246,7 @@ function App() {
             slots={timeSlots}
             currentTime={currentTime}
             changeTime={changeTime}
+            onRemove={onRemove}
             />
         </div>
     </>
